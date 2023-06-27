cd565b50aa583efc1fddce29743ef8bd7fa7b47c
Kirill Grinkov <79636456163@yandex.ru>
2023-06-27 17:38:31 +0400

    addtask29

---
+++ task29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+
+Console.Clear();
+
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное значение массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[] randomArray = new int[size]; 
+
+
+
+ for (int i = 0; i < randomArray.Length; i++)
+    {
+     randomArray[i] = Rand ();
+
+     Console.Write($"{randomArray[i]} ,"); 
+    }
+
+ int Rand()
+ {
+     return new Random().Next(min,max); 
+ }
